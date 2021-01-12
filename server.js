46d55b58fdb295fcf1d7066dca6a81a08f0dd8ad
Cody Snell <codyjsnell@yahoo.com>
2021-01-12 14:44:06 -0600

    creates mailer

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+const port = process.env.PORT || 3000
+
+require('dotenv').config()
+require('./config/database')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SERVICE,
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.WORD
+    }
+})
+// verifying the connection config
+transporter.verify(function(error, success){
+    if (error){
+        console.log(error)
+    } else{
+        console.log("== server is ready to take messages ==")
+    }
+})
+
+app.post('/send', (req, res, next)=>{
+    let mail = {
+        from: `${req.body.emailState.email}`,
+        to: process.env.EMAIL, 
+        subject: `Message From Portfolio: ${req.body.emailState.email}`,
+        text: `${req.body.emailState.email} says, ${req.body.message.message}`
+    }
+    transporter.sendMail(mail, (err, data) =>{
+        if (err){
+            res.json({
+                status:'fail'
+            })
+        } else {
+            res.json({
+                status: 'success'
+            })
+        }
+    })
+})
+
+
+app.use(cors())
+app.use(express.json())
+app.listen(port)
