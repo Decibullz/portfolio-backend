beb6b3d22660000026574aa78f8b913c28cfcbf0
Cody Snell <codyjsnell@yahoo.com>
2021-03-09 15:30:35 -0600

    adds kingdom line kennels to mailer

--- server.js
+++ server.js
@@ -10,71 +10,87 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-
 const transporter = nodemailer.createTransport({
-    host: process.env.SERVICE,
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.WORD
-    }
+  host: process.env.SERVICE,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.WORD,
+  },
 })
 // verifying the connection config
-transporter.verify(function(error, success){
-    if (error){
-        console.log(error)
-    } else{
-        console.log("== server is ready to take messages ==")
-    }
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('== server is ready to take messages ==')
+  }
 })
-
 
 // Personal portfolio
-app.post('/send', (req, res, next)=>{
-    let mail = {
-        from: `${req.body.emailState.email}`,
-        to: process.env.EMAIL, 
-        subject: `Message From Portfolio: ${req.body.emailState.email}`,
-        text: `${req.body.emailState.email} says, ${req.body.message.message}`
+app.post('/send', (req, res, next) => {
+  let mail = {
+    from: `${req.body.emailState.email}`,
+    to: process.env.EMAIL,
+    subject: `Message From Portfolio: ${req.body.emailState.email}`,
+    text: `${req.body.emailState.email} says, ${req.body.message.message}`,
+  }
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail',
+      })
+    } else {
+      res.json({
+        status: 'success',
+      })
     }
-    transporter.sendMail(mail, (err, data) =>{
-        if (err){
-            res.json({
-                status:'fail'
-            })
-        } else {
-            res.json({
-                status: 'success'
-            })
-        }
-    })
+  })
 })
-
 
 // Veronicas lost and found
-app.post('/verosblaf', (req, res, next)=>{
-    let mail = {
-        from: `${req.body.emailState.email}`,
-        to: 'vsnell@bethany-living.com', 
-        subject: `Message from lost and found: ${req.body.emailState.email}`,
-        text: `${req.body.emailState.email} says, ${req.body.message.message}`
+app.post('/verosblaf', (req, res, next) => {
+  let mail = {
+    from: `${req.body.emailState.email}`,
+    to: 'vsnell@bethany-living.com',
+    subject: `Message from lost and found: ${req.body.emailState.email}`,
+    text: `${req.body.emailState.email} says, ${req.body.message.message}`,
+  }
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail',
+      })
+    } else {
+      res.json({
+        status: 'success',
+      })
     }
-    transporter.sendMail(mail, (err, data) =>{
-        if (err){
-            res.json({
-                status:'fail'
-            })
-        } else {
-            res.json({
-                status: 'success'
-            })
-        }
-    })
+  })
 })
 
-
+// Kingdom line Kennels
+app.post('/kingdomlinekennels', (req, res, next) => {
+  let mail = {
+    from: `${req.body.emailState.email}`,
+    to: process.env.EMAIL,
+    subject: `Message From Kingdomlinekennels.com: ${req.body.emailState.email}`,
+    text: `${req.body.emailState.email} says, ${req.body.message.message}`,
+  }
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail',
+      })
+    } else {
+      res.json({
+        status: 'success',
+      })
+    }
+  })
+})
 
 app.use(cors())
 app.use(express.json())
-app.listen(port, ()=> console.log(`up and running on port ${port}`))
+app.listen(port, () => console.log(`up and running on port ${port}`))
